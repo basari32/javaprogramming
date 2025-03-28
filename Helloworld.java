ebbf1effee268c418785f9420d0bd81c11c719bd
Hasanul Basari <147181376+basari32@users.noreply.github.com>
2025-03-28 10:02:30 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+package mca09;
+
+import java.util.Scanner;
+
+public class Helloworld{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s1=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=s1.nextLine();
+		System.out.println("Helloworld "+name);
+	}
+
+}
